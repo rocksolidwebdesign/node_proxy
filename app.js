c5bb2dfe8dac2df4a00e4438eeaf94ba2e1a8929
Vaughn Draughon <vaughn@rocksolidwebdesign.com>
2013-11-13 16:52:32 -0700

    added some large-ish example files for delivery

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.enable('trust proxy');
+
+app.use(express.cookieParser());
+app.use(express.session({secret: '57bef106-4605-429e-83c6-11f456f40f39'}));
+
+app.use(express.compress());
+app.use(express.static('public'));
+app.use(express.logger());
+app.use(express.bodyParser());
+
+app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
 
